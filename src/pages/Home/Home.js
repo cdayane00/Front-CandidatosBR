ed79efa2e454aba905d29e955a1b03f827b3b56c
Carla <carla.dayane.berredo@gmail.com>
2022-09-29 14:42:17 -0400

    feat: page Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,14 @@
-
+import "./styles.css";
+import seta from '../../assets/img/seta.png'
 export default function Home() {
   return (
-    <h1>Sou Home</h1>
+    <div className="container">
+      <h1 className="titulo">Seja bem-vindo(a)</h1>
+      <div className="cargos">
+        <p>Cargos</p>
+        <img className="seta" src={seta} alt="seta" />
+      </div>
+    </div>
+    
   );
 }
